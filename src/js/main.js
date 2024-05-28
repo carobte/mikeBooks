7348c8e4308fda6af3d99df220cd199d37a1c8d0
carobte <caro.bustamante.escobar@gmail.com>
2024-05-28 07:14:25 -0500

    feat: first JSON server functions

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,30 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+
+// Get log in form from index.html
+const loginForm = document.querySelector('#login-form')
+// Get username from index.html
+const usernameLogin = document.querySelector('#username-login')
+// Get password from index.html
+const passwordLogin = document.querySelector('#password-login')
+// Create a const for the API
+
+import { getUsers, validateUsername } from "./api.js"
+
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const user = await validateUsername(usernameLogin.value.toLowerCase())
+    if (!user) {
+        alert("Usuario incorrecto o no estás registrado")
+    } else {
+        if (user.password === passwordLogin.value.toLowerCase()) {
+            alert("Has iniciado sesión") 
+            localStorage.setItem("userLogged",JSON.stringify(user))   
+        } else {
+            alert("Contraseña incorrecta")
+        }
+    }
+    loginForm.reset()
+})
