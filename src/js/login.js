9f5c0b653f9ee6c588c136a86d3e9260a664594a
mspena14 <marlono1naranjo@gmail.com>
2024-05-28 00:30:57 -0500

    feat: add basic login functionality

---
+++ src/js/login.js
@@ -0,0 +1,39 @@
+// Get log in form from index.html
+const loginForm = document.querySelector('#login-form')
+// Get username from index.html
+const usernameLogin = document.querySelector('#username-login')
+// Get password from index.html
+const passwordLogin = document.querySelector('#password-login')
+// Create a const for the API
+const API = "http://localhost:3000/users"
+
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const user = await validateUsername(usernameLogin.value)
+    if (!user) {
+        alert("Usuario incorrecto o no estás registrado")
+    } else {
+        if (user.password === passwordLogin.value) {
+            alert("Has iniciado sesión")    
+        } else {
+            alert("Contraseña incorrecta")
+        }
+    }
+})
+
+
+async function validateUsername(usernameLogin) {
+    const response = await fetch(`${API}?nickname=${usernameLogin}`)
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+    if (data.length === 1) {
+        return data[0]
+    } else {
+        return false
+    }
+}
+
+console.log(loginForm)
+console.log(passwordLogin)
+console.log(usernameLogin)
